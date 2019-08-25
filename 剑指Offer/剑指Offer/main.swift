c9fbe77d08bc29bb1baebb106a36677c3062a130
danny-desktop <dannycaihaoming@gmail.com>
2019-08-25 23:15:10 +0800

    1.update

--- 剑指Offer/剑指Offer/main.swift
+++ 剑指Offer/剑指Offer/main.swift
@@ -11,11 +11,88 @@ import Foundation
 
 //MARK: 面试3：数组中重复的数字
 
-///
-/*
+///题目一：找出数组中重复的数字
+func duplicate(numbers:[Int]) -> Int? {
+    if numbers.isEmpty {
+        return nil
+    }
+    
+    var newNumbers = numbers
+    
+    for (index,value) in newNumbers.enumerated() {
+        if value < 0 || value > newNumbers.count {
+            return nil
+        }
+        //如果下标和数值不一样,就替换数值到对应下标处，如果该下标已经有了，则返回该数值
+        
+        while value != index {
+            if value == newNumbers[value]{
+                return value
+            }
+            
+            let temp = numbers[value]
+            newNumbers[value] = value
+            newNumbers[index] = temp
+        }
+        
+    }
+    
+    return nil
+}
 
- 
+///题目二：不修改数组找出重复的数字
+func getDuplication(numbers:[Int]) -> Int? {
+    
+    if numbers.isEmpty {
+        return nil
+    }
+    
+    //这里的start和end是表示数字的，不是表示数组下标
+    var start = 1
+    var end = numbers.count - 1
+    while start <= end {
+        
+        let mid = (end - start)/2 + start
+        let count = countRange(numbers: numbers, start: start, end: mid)
+        
+        if start == end {
+            ///////似乎二分法最后结束条件都会前等于后
+            if count > 1 {
+                return start
+            }else {
+                break
+            }
+        }
+        
+        if count > mid - start + 1{
+            end = mid
+        }else {
+            start = mid + 1
+        }
+    }
+    
+    
+    return nil
+}
 
- */
+func countRange(numbers:[Int],start:Int,end:Int) -> Int{
+    if numbers.isEmpty {
+        return 0
+    }
+    
+    var count = 0
+    for (_,value) in numbers.enumerated() {
+        if value >= start && value <= end {
+            count += 1
+        }
+    }
+    return count
+}
 
+
+let a = [2,3,1,0,2,5,3]
+let b = [2,3,5,4,3,2,6,7]
+
+let z = getDuplication(numbers: b)
+print(z)
 
