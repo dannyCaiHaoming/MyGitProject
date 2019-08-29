1b91d31a780da37c91b6ed2af5138901e41273ea
danny-desktop <dannycaihaoming@gmail.com>
2019-08-29 23:41:10 +0800

    update UIView相关的笔记

---
+++ iOS面试准备/1.UI视图相关/UIViewProject/UIViewProject/MyCALayer.swift
@@ -0,0 +1,13 @@
+//
+//  MyCALayer.swift
+//  UIViewProject
+//
+//  Created by Danny on 2019/8/29.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+import UIKit
+
+class MyCALayer: CALayer {
+
+}
---
+++ iOS面试准备/1.UI视图相关/UIViewProject/UIViewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewProject
+//
+//  Created by Danny on 2019/8/29.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
