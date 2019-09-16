07d2fe01855bef7bce041c5d6080afdf7c61c0e4
danny-desktop <dannycaihaoming@gmail.com>
2019-09-16 21:52:38 +0800

    上传文件

---
+++ iOS面试准备/4.内存/MemoryProject/MemoryProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryProject
+//
+//  Created by Danny on 2019/9/16.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS面试准备/4.内存/MemoryProject/MemoryProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemoryProject
+//
+//  Created by Danny on 2019/9/16.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
