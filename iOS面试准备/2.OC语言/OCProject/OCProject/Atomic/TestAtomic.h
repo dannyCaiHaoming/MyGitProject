9619058b7eacaea99d233a4fd76205f680719cc8
danny <dannycaihaoming@gmail.com>
2021-06-16 15:02:41 +0800

    提交更新

---
+++ iOS面试准备/2.OC语言/OCProject/OCProject/Atomic/TestAtomic.h
@@ -0,0 +1,19 @@
+//
+//  TestAtomic.h
+//  OCProject
+//
+//  Created by 蔡浩铭 on 2021/6/3.
+//  Copyright © 2021 蔡浩铭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestAtomic : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS面试准备/2.OC语言/OCProject/OCProject/Atomic/TestAtomic.m
@@ -0,0 +1,19 @@
+//
+//  TestAtomic.m
+//  OCProject
+//
+//  Created by 蔡浩铭 on 2021/6/3.
+//  Copyright © 2021 蔡浩铭. All rights reserved.
+//
+
+#import "TestAtomic.h"
+
+@interface TestAtomic ()
+
+@property (atomic, assign) BOOL isTest;
+
+@end
+
+@implementation TestAtomic
+
+@end
