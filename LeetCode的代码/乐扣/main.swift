c083f394bbfe94670945beb1b0eceb481062eb69
dannycaihaoming@gmail.com <dannycaihaoming@gmail.com>
2021-08-23 20:01:28 +0800

    最大回文串

--- LeetCode的代码/乐扣/main.swift
+++ LeetCode的代码/乐扣/main.swift
@@ -305,7 +305,158 @@ func lengthOfLongestSubstring(_ s: String) -> Int {
 
 
 //MARK: 5.最大回文子串
-///动态规划
+/*
+ 给你一个字符串 s，找到 s 中最长的回文子串。
+ 输入：s = "babad"
+ 输出："bab"
+ 解释："aba" 同样是符合题意的答案。
+ */
+
+/*
+ 回文串的规律：
+ 1. 从中心向两边扩充，即只要有一个中心i，然后分别向i-1,i+1判断是否相等
+ 2. 一段长的回文串，减去一头一尾，得到的仍然是回文串,即s[i+1]==s[j-1]
+ 
+ 因此衍生出两种解法：
+ 1. 暴力
+ 2. 动态规划
+ 
+ */
+
+/*
+ 自己写的暴力解法，会超出时间限制。
+ 这个方法的问题在：
+ (1)其实判断分为三种，是有点多余的，其实只需要从i,i+1开始中心扩展即可
+ (2)使用了系统函数，每一步记录的是字符串，且用字符串获取长度的方法，在超长字符串的时候，会超出时间。
+ 
+ 吐了：还是超时
+ */
+func longestPalindrome1(_ s: String) -> String {
+    var largets = String(s[s.index(s.startIndex, offsetBy: 0)])
+    if s.count < 2 {
+        return s
+    } else if s.count == 2 {
+        return s[s.index(s.startIndex, offsetBy: 0)] == s[s.index(s.startIndex, offsetBy: 1)] ? s : largets
+    }
+    
+    for i in 1..<s.count {
+        // 先判断i,i-1 相等
+        // 再判断i-1,i+1相等
+        // 最后判断 1,i+1相等
+        var left = i-1
+        var right = i
+        while left >= 0,
+              right < s.count,
+              s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)] {
+            left -= 1
+            right += 1
+        }
+        right -= 1
+        left += 1
+        if left <= right,
+           s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)],
+           right - left + 1 > largets.count {
+            largets = String(s[s.index(s.startIndex, offsetBy: left)...s.index(s.startIndex, offsetBy: right)])
+        }
+        
+        left = i-1
+        right = i+1
+        while left >= 0,
+              right < s.count,
+              s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)] {
+            left -= 1
+            right += 1
+        }
+        right -= 1
+        left += 1
+        if left <= right,
+           s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)],
+           right - left + 1 > largets.count {
+            largets = String(s[s.index(s.startIndex, offsetBy: left)...s.index(s.startIndex, offsetBy: right)])
+        }
+        
+        left = i
+        right = i+1
+        while left >= 0,
+              right < s.count,
+              s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)] {
+            left -= 1
+            right += 1
+        }
+        right -= 1
+        left += 1
+        if left <= right,
+           s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)],
+           right - left + 1 > largets.count {
+            largets = String(s[s.index(s.startIndex, offsetBy: left)...s.index(s.startIndex, offsetBy: right)])
+        }
+    }
+    return largets
+}
+
+func longestPalindrome1_1(_ s: String) -> String {
+    
+    if s.count < 2 {
+        return s
+    }
+    
+//    func expandCenter(s:String,left: Int,right: Int) -> String {
+//        var t_left = left
+//        var t_right = right
+//        while t_left >= 0,
+//              t_right < s.count,
+//              t_left <= t_right,
+//              s[s.index(s.startIndex, offsetBy: t_left)] == s[s.index(s.startIndex, offsetBy: t_right)]{
+//            t_left -= 1
+//            t_right += 1
+//        }
+//        t_left += 1
+//        t_right -= 1
+//        if t_left >= 0,
+//           t_right < s.count,
+//           t_left <= t_right,
+//           s[s.index(s.startIndex, offsetBy: t_left)] == s[s.index(s.startIndex, offsetBy: t_right)] {
+//            return String(s[s.index(s.startIndex, offsetBy: t_left)...s.index(s.startIndex, offsetBy: t_right)])
+//        }
+//        return ""
+//    }
+    func expandCenterLength(s:String,left: Int,right: Int) -> Int {
+        var t_left = left
+        var t_right = right
+        while t_left >= 0,
+              t_right < s.count,
+              t_left <= t_right,
+              s[s.index(s.startIndex, offsetBy: t_left)] == s[s.index(s.startIndex, offsetBy: t_right)]{
+            t_left -= 1
+            t_right += 1
+        }
+        return t_right - t_left - 1;
+    }
+    
+    
+    var start = 0
+    var end = 0
+    
+    for i in 0..<s.count {
+        // 奇数
+        let a = expandCenterLength(s: s, left: i, right: i)
+        // 偶数
+        let b = expandCenterLength(s: s, left: i, right: i+1)
+            
+        let max = max(a, b)
+        
+        if max > end - start {
+            start = i - (max-1)/2
+            end = i + max/2
+        }
+
+    }
+    
+    return String(s[s.index(s.startIndex, offsetBy: start)...s.index(s.startIndex, offsetBy: end)])
+    
+}
+
+
 func longestPalindrome(_ s: String) -> String {
 	
 	var sChar:[Character] = []
@@ -2510,19 +2661,22 @@ let l1 = ListNode.init(1)
 
 //print(pivotIndex([1, 2, 3]))
 
-class Base: NSObject {
-    @objc  func method5() {print("Base.method5")}
-}
-class Subclass: Base {
-    
-}
+//class Base: NSObject {
+//    @objc  func method5() {print("Base.method5")}
+//}
+//class Subclass: Base {
+//
+//}
+//
+//extension Subclass {
+//    @objc override func method5() {
+//        print("Subclass.method5")
+//    }
+//}
+//
+//
+//let base: Base = Subclass()
+//base.method5()
 
-extension Subclass {
-    @objc override func method5() {
-        print("Subclass.method5")
-    }
-}
 
-
-let base: Base = Subclass()
-base.method5()
+print(longestPalindrome1_1("babad"))
