ff1fe45f91064aedb0c8bc3505b0e5788cfb70b4
danny <dannycaihaoming@gmail.com>
2021-08-27 00:23:08 +0800

    开始快速解决战斗

--- LeetCode的代码/乐扣/数组.swift
+++ LeetCode的代码/乐扣/数组.swift
@@ -13,7 +13,9 @@ class 数组: Do {
         
         let test = 数组()
         
-        print(test.threeSum([0,0,0,0]))
+//        print(test.threeSum([0,0,0,0]))
+        
+        print(test.jump( [2,3,1,1,4]))
     }
     
     //MARK: 1. 两数之和
@@ -32,6 +34,39 @@ class 数组: Do {
         }
         return []
     }
+    
+    //MARK: 45. 跳跃游戏 II
+    /*
+     给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+
+     数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+     你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+     假设你总是可以到达数组的最后一个位置。
+     
+     */
+    
+    /*
+     思路：  从最后一个下标开始，从题干的意思，从前往后找，找到最接近的一个数字达到最后的下标，然后从那个位置继续
+     从头开始找最接近的一个数字达到当下位置。
+     */
+    
+    func jump(_ nums: [Int]) -> Int {
+        var position = nums.count - 1
+        var steps = 0
+        while position > 0 {
+            for i in 0..<position {
+                if i + nums[i] >= position {
+                    position = i
+                    steps += 1
+                    break
+                }
+            }
+        }
+        return steps
+    }
+    
     
     //MARK: 136. 只出现一次的数字
     /*
