9e9054af6bbad33d6b78f66f182ef6fd251201e2
Danny <dannycaihaoming@gmail.com>
2021-11-15 08:46:51 +0800

    增加rxswift

---
+++ 常用开源库源码学习/RxSwift/MyRxSwift/MyRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyRxSwift
+//
+//  Created by 蔡浩铭 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 常用开源库源码学习/RxSwift/MyRxSwift/MyRxSwiftUITests/MyRxSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyRxSwiftUITestsLaunchTests.swift
+//  MyRxSwiftUITests
+//
+//  Created by 蔡浩铭 on 2021/11/13.
+//
+
+import XCTest
+
+class MyRxSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
