e5dc456c546943d2db9df34b78549b507212f096
Danny <dannycaihaoming@gmail.com>
2022-03-12 16:44:12 +0800

    3月12日 leetcode

--- 剑指Offer/CLeetcode/CLeetcode/main.c
+++ 剑指Offer/CLeetcode/CLeetcode/main.c
@@ -14,15 +14,22 @@
 #define INT_MIN  0X80000000
 
 
-// int divide(int a, int b);
-// int findRepeatNumber(int* nums, int numsSize);
-// char* replaceSpace(char* s);
-// int fib(int n);
-// int numWays(int n);
-// int minArray(int* numbers, int numbersSize);
-// bool exist(char** board, int boardSize, int* boardColSize, char* word);
+int divide(int a, int b);
+int findRepeatNumber(int* nums, int numsSize);
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target);
+char* replaceSpace(char* s);
+int fib(int n);
+int numWays(int n);
+int minArray(int* numbers, int numbersSize);
+bool exist(char** board, int boardSize, int* boardColSize, char* word);
 int numberSum(int m);
 int movingCount(int m, int n, int k);
+int cuttingRope(int n);
+int cuttingRope2(int n);
+int hammingWeight(uint32_t n);
+double myPow(double x, int n);
+int* printNumbers(int n, int* returnSize);
+bool isNumber(char* s);
 
 struct ListNode
 {
@@ -45,6 +52,32 @@ int main()
     printf("start --- ");
     //    int a = divide(1,-1);
     // int int_array[7] = {2, 3, 1, 0, 2, 5, 3};
+//    {1,4,7,11,15},
+//    {2,5,8,12,19},
+//    {3,6,9,16,22},
+//    {10,13,14,17,24},
+//    {18,21,23,26,30}
+//    int gridArr[1][1] =
+//    {
+//        {-5},
+//    };
+//    int i, j;
+//    int **grid = (int **) malloc(1 * sizeof(int *));  // 注意：是两个**
+//    for (i = 0; i < 1; i++) {
+//        //grid是6个（5个参数的一维数组）组成的数组
+//        grid[i] = (int *) malloc(1 * sizeof(int));  // 注意：是一个*
+//        for (j = 0; j < 1; j++) {
+//            // 方式一：下标引用
+//            grid[i][j] = gridArr[i][j];
+//
+//            // 方式二：指针计算
+//            // *(*(grid + i) + j) = gridArr[i][j];
+//        }
+//    }
+    
+//    [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+//    int colSize = 1;
+//    int a = findNumberIn2DArray(grid, 1, &colSize, -10);
     // int a = findRepeatNumber(int_array,7);
     // int int_array[3] = {3,1,3};
     
@@ -54,10 +87,27 @@ int main()
     // int a = fib(43);
     // int a = numWays(7);
     
-    int a = movingCount(1,2,1);
+//    int a = movingCount(1,2,1);
     // int a = minArray(int_array,3);
     // printf("%s",a);
+//    int a = cuttingRope(5);
+//    int a = cuttingRope2(120);
+//    int a = hammingWeight(128);
+    
+//    double a = myPow(2.0000, -2147483648);
+//    int size = 9;
+//    int *p = printNumbers(1, &size);
+//
+//    for (int i = 0; i < size; i++) {
+//        printf("CH ---- a %d",p[i]);
+//    }
+    
+
+    bool a = isNumber("2e0");
+    
+//    printf("CH- -- %f",a);
     printf("CH- -- %d",a);
+    
     
     
     printf("end --- ");
@@ -123,6 +173,44 @@ int main()
  int findRepeatNumber(int* nums, int numsSize){
      return 0;
  }
+
+
+/* ########################      04. 二维数组中的查找         ########################*/
+/*
+ 每行的最后一个是行最大，每列的第一个是列最小
+ 可以先取行的最后一个和target判断大小
+ last < target 则行数 + 1继续进行上一步
+ last > target 则列数 -1 继续进行上一步
+ */
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target){
+    if (matrixSize == 0 || *matrixColSize == 0) {
+        return false;
+    }
+    int col = *matrixColSize-1;
+    int row = 0;
+    while (1) {
+        if (row >= matrixSize) {
+            return false;
+        }
+        while (1) {
+            if (col < 0) {
+                return false;
+            }
+            int cur = matrix[row][col];
+            if (cur == target) {
+                return true;
+            }
+            else if (cur < target) {
+                row += 1;
+                break;
+            }else {
+                col -= 1;
+                break;
+            }
+        }
+    }
+    return false;
+}
 
 
  /* ########################      05. 替换空格         ########################*/
@@ -442,7 +530,7 @@ int main()
 
 
 
-/* ########################      剑指 Offer 13. 机器人的运动范围        ########################*/
+/* ########################      13. 机器人的运动范围        ########################*/
 /*
  构建一个二维数组记录当前位置不能走，然后递归上下左右四个方向，用最大值和当前加一
  */
@@ -501,3 +589,245 @@ int movingCount(int m, int n, int k)
     movingCount_dfs(visit, m, n, 0, 0, k,&count);
     return count;
 }
+
+
+
+/* ########################       14- I. 剪绳子        ########################*/
+/*
+ 1  1
+ 2  1x1
+ 3  1x2  1x1x1
+ 4  1x3  2x2  1x1x1
+ 5  1x4 2x3  2x2x1  1x1x3
+ 6  1x5  2x2x2  2x3x1  2x4  3x3
+ 7  3x4
+ 8  3x3x2
+ 10 3x3x4
+ 
+ 从规律可以得到尽量划分成3，如果遇到4，就分成2x2,
+ */
+int cuttingRope_cache[60] ={0};
+int cuttingRope(int n){
+    if (n == 2) {
+        cuttingRope_cache[2] = 1;
+        return 1;
+    }
+    if (n == 3) {
+        cuttingRope_cache[3] = 2;
+        return 2;
+    }
+    if (n == 4) {
+        cuttingRope_cache[4] = 4;
+        return 4;
+    }
+    if (cuttingRope_cache[n] != 0) {
+        return cuttingRope_cache[n];
+    }
+    int next = 0;
+    if (n-3 < 4) {
+        next = n-3;
+    }else {
+        next = cuttingRope(n-3);
+    }
+    int result = 3 * next;
+    cuttingRope_cache[n] = result;
+    return result;
+}
+
+
+
+/* ########################       14- II. 剪绳子 II        ########################*/
+/*
+ 这道题的答案范围会越界，因此处理上需要先考虑越界的情况，将数据类型改成long，然后取模操作后再重新赋值会int
+ */
+int cuttingRope2(int n){
+    if (n == 2) {
+        return 1;
+    }
+    if (n == 3) {
+        return 2;
+    }
+    if (n == 4) {
+        return 4;
+    }
+    int next = 0;
+    if (n-3 < 4) {
+        next = n-3;
+    }else {
+        next = cuttingRope(n-3);
+    }
+    long temp = (long)next * 3 % 1000000007;
+    int result = (int)temp ;
+    return result;
+}
+
+
+/* ########################       15. 二进制中1的个数        ########################*/
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+        count += n & 1;
+        n = n >> 1;
+    }
+    return count;
+}
+
+
+/* ########################       16. 数值的整数次方        ########################*/
+/*
+ 这种的大数算法，一次次遍历会超时。因此需要寻找能偷鸡的办法，幂次方的话，就可以每次计算减半的幂次方,
+ 同时需要注意这里，幂次数小于0，就得被1除!
+ */
+double pow1(double x, long long n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n == 1) {
+        return x;
+    }
+    // 能对半开就half*half
+    // 不能的话 就是对半开还多一个自己，例如3次方 就是half*half*x
+    double halfPow = pow1(x, n/2);
+    return n % 2 == 0 ? halfPow*halfPow : halfPow * halfPow * x;
+}
+double myPow(double x, int n)
+{
+    if (x == 0 && n < 0) {
+        return 0.0;
+    }
+    long long ln = n;
+    return n < 0 ? 1/pow1(x, -ln) : pow1(x, ln);
+}
+
+
+/* ########################       17. 数值的整数次方        ########################*/
+/*
+ 1 1~9
+ 3  1~999
+ */
+int* printNumbers(int n, int* returnSize){
+    int count = 1;
+    for (int i = 0; i < n; i++) {
+        count *= 10;
+    }
+    count -= 1;
+    *returnSize = count;
+    int *p = (int *)malloc(sizeof(int) * count);
+    for (int i = 0; i < *returnSize; i++) {
+        *(p+i) = i+1;
+    }
+    return p;
+}
+
+
+/* ########################       18. 删除链表的节点        ########################*/
+struct ListNode* deleteNode(struct ListNode* head, int val){
+    struct ListNode* temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (head->val == val) {
+        temp = head->next;
+        head->next = NULL;
+        return temp;
+    }
+    struct ListNode* first = (struct ListNode *)malloc(sizeof(struct ListNode));
+    first = head;
+    while (head->next != NULL) {
+        if (head->next->val == val) {
+            temp = head->next;
+            head->next = temp->next;
+            break;
+        }
+        head = head->next;
+    }
+    return first;
+}
+
+
+
+/* ########################       20. 表示数值的字符串        ########################*/
+/*
+ 模式匹配：
+ 1.  开头能是符号或者字母或者数字
+    1.  如果是数字
+        a. 可以继续是数字
+        b. 可以接字母，但是字母后面必须是符号,符号后就得是数字
+        c. 可以接小数点，但是只有一个
+    2.  如果是符号
+        a. 只能继续是数字
+    
+ */
+bool isNumber(char* s) {
+    int len = strlen(s);
+    bool hasNum = false, hasE = false, hasSign = false, hasDot = false;
+    int index = 0;
+    /* 去除前导空格 */
+    while (index < len && s[index] == ' ') {
+        index++;
+    }
+    /* 从头到尾遍历字符串 */
+    while (index < len) {
+        /* 如果当前字符c是数字：将hasNum置为true，
+         * index往后移动一直到非数字或遍历到末尾位置；
+         * 如果已遍历到末尾(index == n)，结束循环
+         */
+        while (index < len && s[index] >= '0' && s[index] <= '9') {
+            index++;
+            hasNum = true;
+        }
+        if (index == len) {
+            break;
+        }
+        char c = s[index];
+        if (c == 'e' || c == 'E') {
+            /* 如果当前字符c是'e'或'E'：
+            * 如果e已经出现或者当前e之前没有出现过数字，返回fasle；
+            * 否则令hasE = true，并且将其他3个flag全部置为false，
+            * 因为要开始遍历e后面的新数字了
+            */
+            if (hasE || !hasNum) {
+                return false;
+            }
+            hasE = true;
+            hasDot = hasNum = hasSign = false;
+        } else if (c == '+' || c == '-') {
+            /* 如果当前字符c是+或-：如果已经出现过+或-
+             * 或者已经出现过数字或者已经出现过'.'，返回flase；
+             * 否则令hasSign = true
+             */
+            if (hasSign || hasNum || hasDot) {
+                return false;
+            }
+            hasSign = true;
+        } else if (c == '.') {
+            /* 如果当前字符c是'.'：
+             * 如果已经出现过'.'或者已经出现过'e'或'E'，返回false；
+             * 否则令hasDot = true
+             */
+            if (hasDot || hasE) {
+                return false;;
+            }
+            hasDot = true;
+        } else if (c == ' ') {
+            /* 如果当前字符c是' '：
+             * 结束循环，因为可能是末尾的空格了，
+             * 但也有可能是字符串中间的空格，在循环外继续处理
+             */
+            break;
+        } else {
+            /* 如果当前字符c是除了上面5种情况以外的其他字符，直接返回false */
+            return false;
+        }
+        index++;
+    }
+    /* 如果当前索引index与字符串长度相等，说明遍历到了末尾，
+     * 但是还要满足hasNum为true才可以最终返回true，
+     * 因为如果字符串里全是符号没有数字的话是不行的，而且e后面没有数字也是不行的，
+     * 但是没有符号是可以的，所以4个flag里只要判断一下hasNum就行；
+     * 所以最后返回的是hasNum && index == n
+     */
+    while (index < len && s[index] == ' ') {
+        index++;
+    }
+    return hasNum && index == len;
+}
+
+
