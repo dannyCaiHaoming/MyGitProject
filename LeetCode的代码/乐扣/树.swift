d45f6fe8d688a93ac239cff74b97ea0b36713f9b
danny_home <dannycaihaoming@163.com>
2024-02-26 22:00:16 +0800

    upload

--- LeetCode的代码/乐扣/树.swift
+++ LeetCode的代码/乐扣/树.swift
@@ -40,8 +40,13 @@ class 树: Do {
         
             
         let test =  树()
-        test.isSymmetric(a_1)
+//        test.isSymmetric(a_1)
         
+        let r = test.deserialize("1,null,2")
+        
+        let r1 = test.serialize(r)
+        print(r)
+        print(r1)
         
         
     }
@@ -239,6 +244,52 @@ class 树: Do {
         return count
     }
     
+    func maxDepth1(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        let left = maxDepth(root.left)
+        let right = maxDepth(root.right)
+        if left > right {
+            return left + 1
+        } else {
+            return right + 1
+        }
+        
+    }
+    
+    
+    // MARK: 105. 从前序与中序遍历序列构造二叉树
+    
+    /*
+     从知识点知道，前序中第一位，是根节点，然后拿着根节点去中序，进行拆分。
+     分成左子树和右子树。
+     所以，每次从前序中，取出一位，然后到中序查找，然后将中序分成左子树，和右子树。
+     那么构造一个root，那他的左子树，也能用同样的算法，计算，右子树，也是能用同样的算法计算。
+     只是要规划好，根据前序得到根节点后，需要用中序，遍历出根节点位置，然后划分好左右子树，然后进行递归。
+     */
+    func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+        guard !preorder.isEmpty,
+              !inorder.isEmpty,
+              let rootV = preorder.first else {
+            return nil
+        }
+        let count = preorder.count
+        let root = TreeNode.init(rootV)
+        var mid = 0
+        while inorder[mid] != rootV {
+            mid += 1
+        }
+        let leftCount = mid
+        let rightCount = count - leftCount - 1
+        
+        let left = buildTree(Array(preorder[1..<leftCount+1]), Array(inorder[0..<leftCount]))
+        let right = buildTree(Array(preorder[leftCount+1..<count]), Array(inorder[leftCount+1..<count]))
+        root.left = left
+        root.right = right
+        return root
+    }
+    
     
     //MAKR:  112. 路径总和
     /*
@@ -255,7 +306,101 @@ class 树: Do {
         hasPathSum(root.right, targetSum - root.val)
 
     }
+    
+    
+    //MARK: 114. 二叉树展开为链表
+    /*
+     看成了中序遍历。
+     是先序遍历的顺序。
+     
+    貌似迭代方法的时候，先入根节点，然后再左右，根再右左，就是不一样的遍历方式。
+     
+     ** 官方题解，类似双指针，也是链表常用，一个指向上一个节点，用栈存储下一个将要访问的节点。 **
+     */
+    
+    func flatten(_ root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        var array:[TreeNode] = preFlatten(root: root)
+        
+        for j in 0..<array.count {
+            if j+1 < array.count {
+                array[j].left = nil
+                array[j].right = array[j+1]
+            }
+        }
+    }
+    
+    func preFlatten(root: TreeNode?) -> [TreeNode] {
+        guard let root = root else {
+            return []
+        }
+        let left = preFlatten(root: root.left)
+        let right = preFlatten(root: root.right)
+        return [root] + left + right
+    }
+    
+    func flatten2(_ root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        var pre: TreeNode? = nil
+        var stack: [TreeNode] = []
+        stack.append(root)
+        while !stack.isEmpty  {
+            let cur = stack.removeLast()
+            if let r = cur.right {
+                stack.append(r)
+            }
+            if let l = cur.left {
+                stack.append(l)
+            }
+            if pre != nil {
+                pre?.left = nil
+                pre?.right = cur
+                pre = cur
+            } else {
+                pre?.left = nil
+                pre = cur
+            }
+        }
+    }
 
+    
+    // MARK: 124. 二叉树中的最大路径和
+    /*
+     想复杂了。
+     */
+    var maxPathSumResult = Int.min
+    func maxPathSum(_ root: TreeNode?) -> Int {
+        _ = maxPathSumTrace(root: root)
+        return maxPathSumResult
+    }
+    
+    func maxPathSumTrace(root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var left = 0
+        let l = maxPathSumTrace(root: root.left)
+        if l > left {
+            left = l
+        }
+        var right = 0
+        let r = maxPathSumTrace(root: root.right)
+        if r > right {
+            right = r
+        }
+        // 根节点的总值，是左到右。但是最后到用左还是右的时候需要判断
+        let cur = root.val + left + right
+        if cur > maxPathSumResult {
+            maxPathSumResult = cur
+        }
+        // 判断用左边还是右边。
+        return root.val + left > root.val + right ? root.val + left : root.val + right
+        
+    }
     
     //MARK: 144. 二叉树的前序遍历
     /*
@@ -303,7 +448,7 @@ class 树: Do {
     }
     
     
-    //MARK: 235. 二叉搜索树的最近公共祖先
+    //MARK: 236. 二叉搜索树的最近公共祖先
     /*
      给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
 
@@ -311,8 +456,133 @@ class 树: Do {
 
      例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
      */
+    var ancestor: TreeNode?
     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-        return nil
+        _ = lowestCommonAncestorTrace(root, p, q)
+        return ancestor
+    }
+    
+    func lowestCommonAncestorTrace(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        /*
+         如果根节点等于p或q，并且左子树存在，或者右子树存在pq
+         或者左子树右子树存在pq
+         */
+        if root == nil {
+            return false
+        }
+        let rootSame = root?.val == p?.val || root?.val == q?.val
+        let sameLeft = lowestCommonAncestorTrace(root?.left, p, q)
+        let sameRight = lowestCommonAncestorTrace(root?.right, p, q)
+        if (rootSame && (sameLeft || sameRight)) || (sameLeft && sameRight) {
+            ancestor = root
+        }
+        return rootSame || sameLeft || sameRight
+    }
+    
+    
+    // MARK: 297 二叉树的序列化和反序列化
+    func serialize(_ root: TreeNode?) -> String {
+        var stack: [TreeNode?] = []
+        guard let root = root else {
+            return ""
+        }
+        stack.append(root)
+        var result = ""
+        while !stack.isEmpty  {
+            let cur = stack.removeFirst()
+            if cur != nil {
+                if result.isEmpty {
+                    result = "\(cur!.val)"
+                } else {
+                    result = result + "," + "\(cur!.val)"
+                }
+                var hasNext = true
+                if stack.last??.left == nil && stack.last??.right == nil && cur?.right == nil {
+                    hasNext = false
+                }
+                if let l = cur?.left {
+                    stack.append(l)
+                } else {
+                    if hasNext {
+                        stack.append(nil)
+                    }
+                }
+                if let r = cur?.right {
+                    stack.append(r)
+                } else {
+                    if hasNext {
+                        stack.append(nil)
+                    }
+                }
+            } else {
+                result = result + "," + "null"
+            }
+
+            /*
+             有个问题，4,5的子节点，怎么判断不需要加。
+             */
+        }
+        return result
+    }
+    
+    func deserialize(_ data: String) -> TreeNode? {
+        if data.isEmpty {
+            return nil
+        }
+        let arr = data.components(separatedBy: ",")
+        /*
+         递归。扔下一个n+1，返回根节点
+         */
+        return deserializeTrace(arr, n: 0)
+    }
+    
+    func deserializeTrace(_ datas: [String],n: Int) -> TreeNode? {
+        if datas.isEmpty {
+            return nil
+        }
+        if n >= datas.count {
+            return nil
+        }
+        let word = datas[n]
+        if word == "null" {
+            return nil
+        }
+        let cur = TreeNode(Int(word)!)
+        let left = deserializeTrace(datas, n: 2*n+1)
+        let right = deserializeTrace(datas, n: 2*n+2)
+        cur.left = left
+        cur.right = right
+        return cur
+    }
+    
+    // MARK: 337 打家劫舍iii
+    /*
+     需要间隔。每一层递归，跳两层或者三层
+     
+     cur = nil，可以走一步或2步
+     cur != nil，可以走2步或者3步
+     */
+    func rob(_ root: TreeNode?) -> Int {
+        let step0 = robTrace(root: root)
+        let step1 = robTrace(root: root?.left) + robTrace(root: root?.right)
+        return step0 > step1 ? step0 : step1
+    }
+    
+    func robTrace(root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        // 跳2
+        let l1 = robTrace(root: root.left?.left)
+        let l2 = robTrace(root: root.left?.right)
+        let r1 = robTrace(root: root.right?.left)
+        let r2 = robTrace(root: root.right?.right)
+        // 跳3
+        let l3 = robTrace(root: root.left?.left)
+        let l4 = robTrace(root: root.left?.right)
+        let r5 = robTrace(root: root.right?.left)
+        let r6 = robTrace(root: root.right?.right)
+        return 0
     }
     
     //MARK: 653. 两数之和 IV - 输入 BST
